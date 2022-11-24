4182672abe654fc2b61b900a35eb065a6a10ec18
piko <piko_git@ithea.de>
2022-11-24 16:05:54 +0100

    Add syntax highlighting for checked boxes

--- src/Editor.svelte
+++ src/Editor.svelte
@@ -46,7 +46,7 @@
         }
     }
 
-    function checkBoxOverlay() {
+    function checkBoxEmptyOverlay() {
         const query = /\[ \]/g
 
         return {
@@ -55,7 +55,26 @@
                 var match = query.exec(stream.string)
                 if (match && match.index == stream.pos) {
                     stream.pos += match[0].length || 1
-                    return "checkbox"
+                    return "checkbox-empty"
+                } else if (match) {
+                    stream.pos = match.index
+                } else {
+                    stream.skipToEnd()
+                }
+            },
+        }
+    }
+
+    function checkBoxFilledOverlay() {
+        const query = /\[x\]/g
+
+        return {
+            token: function (stream) {
+                query.lastIndex = stream.pos
+                var match = query.exec(stream.string)
+                if (match && match.index == stream.pos) {
+                    stream.pos += match[0].length || 1
+                    return "checkbox-filled"
                 } else if (match) {
                     stream.pos = match.index
                 } else {
@@ -93,7 +112,8 @@
             })
 
             editor.addOverlay(urlOverlay())
-            editor.addOverlay(checkBoxOverlay())
+            editor.addOverlay(checkBoxEmptyOverlay())
+            editor.addOverlay(checkBoxFilledOverlay())
 
             editor.getWrapperElement().addEventListener("mousedown", (e) => {
                 if (e.which == 1) {
@@ -263,12 +283,15 @@
         color: darkblue !important;
         text-decoration: none !important;
     }
-    :global(.cm-checkbox) {
+    :global(.cm-checkbox-empty),
+    :global(.cm-checkbox-filled) {
         cursor: pointer;
         font-weight: bold;
+        text-decoration: none !important;
+    }
+    :global(.cm-checkbox-empty) {
         color: white !important;
         background: #c12f2f !important;
-        text-decoration: none !important;
     }
     :global(.remote-caret:hover > div) {
         visibility: visible;
