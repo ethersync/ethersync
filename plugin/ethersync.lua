b69b8e36fbb695fdeccad12690445979b40e4dfc
blinry <mail@blinry.org>
2023-03-25 11:47:41 +0100

    Connect to TCP server and receive updates

--- plugin/ethersync.lua
+++ plugin/ethersync.lua
@@ -1,12 +1,19 @@
-ignored_ticks = {}
+local ignored_ticks = {}
+local sep = "\t"
 
 local ns_id = vim.api.nvim_create_namespace('Ethersync')
 local virtual_cursor
+local server = vim.loop.new_tcp()
 
 function indexToRowCol(index)
     local row = vim.fn.byte2line(index+1) - 1
     local col = index - vim.api.nvim_buf_get_offset(0, row)
     return row, col
+end
+
+function ignoreNextUpdate()
+    local nextTick = vim.api.nvim_buf_get_changedtick(0)
+    ignored_ticks[nextTick] = true
 end
 
 function rowColToIndex(row, col)
@@ -15,12 +22,14 @@ end
 
 function insert(index, content)
     local row, col = indexToRowCol(index)
+    ignoreNextUpdate()
     vim.api.nvim_buf_set_text(0, row, col, row, col, vim.split(content, "\n"))
 end
 
 function delete(index, length)
     local row, col = indexToRowCol(index)
     local rowEnd, colEnd = indexToRowCol(index + length)
+    ignoreNextUpdate()
     vim.api.nvim_buf_set_text(0, row, col, rowEnd, colEnd, {""})
 end
 
@@ -52,6 +61,8 @@ function EtherSync()
 
     setCursor(12,10)
 
+    connect()
+
     local buf = vim.api.nvim_get_current_buf()
     local row, col = unpack(vim.api.nvim_win_get_cursor(0))
     vim.api.nvim_buf_attach(0, false, {
@@ -65,23 +76,56 @@ function EtherSync()
             local new_content_lines = vim.api.nvim_buf_get_text(buffer_handle, start_row, start_column, start_row+new_end_row, start_column+new_end_column, {})
             local changed_string = table.concat(new_content_lines, "\n")
 
+            if new_end_byte_length >= old_end_byte_length then
+                server:write("insert" .. sep .. byte_offset .. sep .. changed_string)
+            else
+                server:write("delete" .. sep .. byte_offset .. sep .. old_end_byte_length - new_end_byte_length)
+            end
+
             -- For testing, insert text at the virtual cursor.
-            vim.schedule(function()
-                local nextTick = vim.api.nvim_buf_get_changedtick(buffer_handle)
-                ignored_ticks[nextTick] = true
+            --vim.schedule(function()
 
-                local row, col = unpack(vim.api.nvim_buf_get_extmark_by_id(0, ns_id, virtual_cursor, {}))
-                local index = rowColToIndex(row, col)
+            --    local row, col = unpack(vim.api.nvim_buf_get_extmark_by_id(0, ns_id, virtual_cursor, {}))
+            --    local index = rowColToIndex(row, col)
 
-                if new_end_byte_length >= old_end_byte_length then
-                    insert(index, changed_string)
-                else
-                    local length = old_end_byte_length - new_end_byte_length
-                    delete(index-length, length)
-                end
-            end)
+            --    if new_end_byte_length >= old_end_byte_length then
+            --        insert(index, changed_string)
+            --    else
+            --        local length = old_end_byte_length - new_end_byte_length
+            --        delete(index-length, length)
+            --    end
+            --end)
         end
     })
+end
+
+function connect()
+    server:connect("127.0.0.1", 9000, function (err)
+      -- check error and carry on.
+    end)
+    server:read_start(function(err, data)
+        if err then
+            print(err)
+            return
+        end
+        if data then
+            print(data)
+            local parts = vim.split(data, sep)
+            if parts[1] == "insert" then
+                local index = tonumber(parts[2])
+                local content = parts[3]
+                vim.schedule(function()
+                    insert(index, content)
+                end)
+            elseif parts[1] == "delete" then
+                local index = tonumber(parts[2])
+                local length = tonumber(parts[3])
+                vim.schedule(function()
+                    delete(index, length)
+                end)
+            end
+        end
+    end)
 end
 
 -- When new buffer is loaded, run EtherSync.
