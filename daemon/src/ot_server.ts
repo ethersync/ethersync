2d1e0483f226f0d3195f92fd735af1b04a1936ee
blinry <mail@blinry.org>
2023-12-11 16:39:54 +0100

    Add a big diagram to the OTServer class

--- daemon/src/ot_server.ts
+++ daemon/src/ot_server.ts
@@ -1,7 +1,57 @@
 import {cloneDeep} from "lodash"
 
-// Can receive operations from both the CRDT world, and one editor.
-// It will make sure to send the correct operations back to them using the provided callbacks.
+/*
+    This class receive operations from both the CRDT world, and one editor.
+    It will make sure to send the correct operations back to them using the provided callbacks.
+
+    Here's an example of how it works:
+
+    1. The daemon starts with an empty document.
+    2. It applies the d1 operation to it, which the editor also receives and applies.
+    3. The daemon applies d2 and d3, and sends them to the editor (thinking these would put it
+       into the same state).
+       It also sends along the editor revision, which the number of ops received by the editor,
+       which is basically the column, and specifies the point the ops apply to. (Here: 0).
+    4. But the editor has made concurrent edits e1 and e2 in the meantime. It rejects d2 and d3.
+       It sends e1 and e2 to the daemon, along with the daemon revision, which is the number of ops
+       it has received from the daemon (the row).
+    5. The daemon transforms e1 and e2 through d2 and d3, creating e1' and e2', and applies them
+       to the document. It sends d2' and d3' to the editor, along with the editor revision (2).
+    6. The editor receives d2' and applies it, but then makes edit e3, and sends it to the daemon.
+       It rejects d3' which it receives later.
+    7. The daemon meanwhile makes edit d4. Upon reciving e3, it transforms it against d3' and d4,
+       and sends d3'' and d4' to the editor. It applies d4 and e3'' to the document.
+    8. The editor receives d3'' and d4', and applies them. Both sides now have the same document.
+
+
+     ---- the right axis is the editor revision --->
+    |
+    | the down axis is the daemon revision
+    |
+    v
+
+        *
+        |
+     d1 |
+        v  e1      e2
+        * ----> * ----> *
+        |       |       |
+     d2 |       |       | d2'
+        v       v       v  e3
+        * ----> * ----> * ----> *          Ops in the rightmost column need
+        |       |       |       |          to be queued by us, because
+     d3 |       |       | d3'   | d3''     we don't know whether the
+        v  e1'  v  e2'  v  e3'  v          editor accepted them. (d3'' and d4')
+        * ----> * ----> * ----> *
+                        |       |
+    The lower        d4 |       | d4'
+    zig-zag is          v  e3'' v
+    the operations      * ----> *
+    log saved by the
+    daemon.
+    (d1, d2, d3, e1', e2', d4, e3'')
+
+*/
 export class OTServer {
     // "Source of truth" operations.
     operations: Operation[] = []
