e75852c26b883589643d76696bf28e8763bab4ee
blinry <mail@blinry.org>
2024-03-25 14:56:46 +0100

    Move "it works" test to main.rs
    I think lib.rs is used for libraries that other people should use after
    all.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -33,3 +33,13 @@ fn main() -> io::Result<()> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
