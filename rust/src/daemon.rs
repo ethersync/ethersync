7441bd6c18e9a755334d96e1d86e94f00d3a9427
blinry <mail@blinry.org>
2024-03-26 14:55:31 +0100

    Allow multiple threads to access Automerge doc via an Arc<Mutex<T>>
    The peer sync state is given access to in a similar way.

--- rust/src/daemon.rs
+++ rust/src/daemon.rs
@@ -7,20 +7,22 @@ use jsonrpc_core::IoHandler;
 use rand::{distributions::Alphanumeric, Rng};
 use std::fs;
 use std::io;
-use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::os::unix::net::UnixListener;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 const SOCKET_PATH: &str = "/tmp/ethersync";
 
 pub struct Daemon {
-    doc: automerge::AutoCommit,
+    doc: Arc<Mutex<automerge::AutoCommit>>,
 }
 
 impl Daemon {
     pub fn new() -> Self {
         let doc = AutoCommit::new();
+        let doc = Arc::new(Mutex::new(doc));
 
         Self { doc }
     }
@@ -58,31 +60,34 @@ impl Daemon {
     fn init_text(&mut self) {
         let _text = self
             .doc
+            .lock()
+            .unwrap()
             .put_object(automerge::ROOT, "text", ObjType::Text)
             .unwrap();
     }
 
-    fn edit_text(&mut self) {
-        let text = self.doc.get(automerge::ROOT, "text").unwrap();
+    fn edit_text(doc: Arc<Mutex<AutoCommit>>) {
+        println!("Trying to edit text!");
+        let mut doc = doc.lock().unwrap();
+        println!("Editing text!");
+        let text = doc.get(automerge::ROOT, "text").unwrap();
         if let Some((automerge::Value::Object(ObjType::Text), text)) = text {
-            let text_length = self.doc.text(&text).unwrap().len();
+            let text_length = doc.text(&text).unwrap().len();
             let random_string: String = rand::thread_rng()
                 .sample_iter(&Alphanumeric)
                 .take(1)
                 .map(char::from)
                 .collect();
             let random_position = rand::thread_rng().gen_range(0..(text_length + 1));
-            self.doc
-                .insert(text, random_position, random_string)
-                .unwrap();
+            doc.insert(text, random_position, random_string).unwrap();
         } else {
-            panic!("Text object not found.");
+            println!("Text object not found.");
         }
     }
 
     pub fn listen_tcp(&mut self) -> io::Result<()> {
         self.init_text();
-        self.edit_text();
+        //Self::edit_text(&mut self.doc);
 
         let listener = TcpListener::bind("0.0.0.0:4242").unwrap();
         println!("Listening on TCP port: {}", listener.local_addr().unwrap());
@@ -92,11 +97,7 @@ impl Daemon {
 
             // TODO: Allow more than one peer to dial us at the same time.
             println!("Peer dialed us.");
-            //let result = self.sync_with_peer(&mut stream, false);
             self.start_sync(stream)?;
-            //if let Err(e) = result {
-            //    println!("Error: {:?}", e);
-            //}
         }
 
         Ok(())
@@ -107,92 +108,93 @@ impl Daemon {
         let stream = TcpStream::connect(addr)?;
         //let result = self.sync_with_peer(&mut stream, true);
         self.start_sync(stream)?;
-        //if let Err(e) = result {
-        //    println!("Error: {:?}", e);
-        //}
         Ok(())
     }
 
     fn start_sync(&mut self, stream: TcpStream) -> io::Result<()> {
+        let peer_state = SyncState::new();
+        let peer_state = Arc::new(Mutex::new(peer_state));
+
         let stream2 = stream.try_clone().unwrap();
 
+        let peer_state_clone = peer_state.clone();
+        let doc_clone = self.doc.clone();
         thread::spawn(move || {
-            Self::sync_receive(stream).unwrap();
+            Self::sync_receive(stream, doc_clone, peer_state_clone).unwrap();
         });
 
-        Self::sync_send(stream2).unwrap();
+        let doc_clone = self.doc.clone();
+        thread::spawn(move || loop {
+            {
+                let doc_clone = doc_clone.clone();
+                Self::edit_text(doc_clone);
+                thread::sleep(std::time::Duration::from_secs(5));
+            }
+        });
+
+        let doc_clone = self.doc.clone();
+        Self::sync_send(stream2, doc_clone, peer_state).unwrap();
 
         Ok(())
     }
 
-    fn sync_receive(mut reader: TcpStream) -> io::Result<()> {
+    fn sync_receive(
+        mut reader: TcpStream,
+        doc: Arc<Mutex<AutoCommit>>,
+        state: Arc<Mutex<SyncState>>,
+    ) -> io::Result<()> {
         loop {
-            //let peer_state = SyncState::new();
             let mut message_len_buf = [0; 4];
             reader.read_exact(&mut message_len_buf)?;
-            let number = i32::from_be_bytes(message_len_buf);
-            println!("Received number: {:?}", number);
-        }
-    }
+            let message_len = i32::from_be_bytes(message_len_buf);
+            let mut message_buf = vec![0; message_len as usize];
+            reader.read_exact(&mut message_buf)?;
+            let message = Message::decode(&message_buf).unwrap();
+            println!("Received message: {:?}", message);
 
-    fn sync_send(mut writer: TcpStream) -> io::Result<()> {
-        loop {
-            let random_number: i32 = rand::thread_rng().gen_range(0..100);
-            println!("Sending number: {:?}", random_number);
-            writer.write_all(&random_number.to_be_bytes())?;
-            thread::sleep(std::time::Duration::from_secs(1));
-        }
-    }
+            println!("Trying to lock doc");
+            let mut doc = doc.lock().unwrap();
+            let mut state = state.lock().unwrap();
+            println!("Locked doc");
 
-    // TODO: Build this in an "event"-based way, where peers can send and receive independently at
-    // any time.
-    fn sync_with_peer(
-        &mut self,
-        stream: &mut TcpStream,
-        mut skip_first_send: bool,
-    ) -> io::Result<()> {
-        let mut peer_state = SyncState::new();
-        loop {
-            // Send sync message.
-            if skip_first_send {
-                skip_first_send = false;
-            } else if let Some(message) = self.doc.sync().generate_sync_message(&mut peer_state) {
-                let message_buf = message.encode();
-                let message_len = message_buf.len() as u32;
-                stream.write_all(&message_len.to_be_bytes())?;
-                stream.write_all(&message_buf)?;
-            } else {
-                let message_len = 0i32;
-                stream.write_all(&message_len.to_be_bytes())?;
-            }
+            doc.sync()
+                .receive_sync_message(&mut state, message)
+                .unwrap();
 
-            // Receive sync message.
-            let mut message_len_buf = [0; 4];
-            stream.read_exact(&mut message_len_buf)?;
-            let message_len = u32::from_be_bytes(message_len_buf);
-            if (message_len as usize) == 0 {
-            } else {
-                let mut message_buf = vec![0; message_len as usize];
-                stream.read_exact(&mut message_buf)?;
-                let message = Message::decode(&message_buf).unwrap();
-                self.doc
-                    .sync()
-                    .receive_sync_message(&mut peer_state, message)
-                    .unwrap();
-            }
-            let text = self.doc.get(automerge::ROOT, "text").unwrap();
+            let text = doc.get(automerge::ROOT, "text").unwrap();
             if let Some((automerge::Value::Object(ObjType::Text), text)) = text {
-                println!("My text is now: {}", self.doc.text(&text).unwrap());
-
-                // Sometimes edit the text.
-                if rand::thread_rng().gen_bool(0.5) {
-                    self.edit_text();
-                }
+                println!("My text is now: {}", doc.text(&text).unwrap());
             } else {
                 println!("I don't have text yet.");
             }
+        }
+    }
 
-            thread::sleep(std::time::Duration::from_secs(1));
+    fn sync_send(
+        mut writer: TcpStream,
+        doc: Arc<Mutex<AutoCommit>>,
+        state: Arc<Mutex<SyncState>>,
+    ) -> io::Result<()> {
+        loop {
+            let message_maybe = {
+                let mut doc = doc.lock().unwrap();
+                let mut state = state.lock().unwrap();
+
+                let message = doc.sync().generate_sync_message(&mut state);
+                message
+            };
+
+            if let Some(message) = message_maybe {
+                let message_buf = message.encode();
+                let message_len = message_buf.len() as i32;
+                writer.write_all(&message_len.to_be_bytes())?;
+                writer.write_all(&message_buf)?;
+
+                println!("Sent message: {:?}", &message_buf);
+            } else {
+                thread::sleep(std::time::Duration::from_secs(1));
+                println!("No message to send.");
+            };
         }
     }
 }
