ethersync/ethersync

commit 9502c7283bb2456bac455fa62bcaeb2a9e7b9659

Apply some clippy suggestions in ot and types

rust/src/types.rs
@@ -215,7 +215,7 @@ impl From<EditorTextDelta> for TextDelta {
         for ed_op in ed_delta.0 {
             let mut delta_step = TextDelta::default();
             if ed_op.range.is_empty() {
-                if ed_op.replacement != "" {
+                if ed_op.replacement.is_empty() {
                     // insert
                     delta_step.retain(ed_op.range.anchor);
                     delta_step.insert(&ed_op.replacement);

rust/src/ot.rs
@@ -142,10 +142,12 @@ impl OTServer {
             if op_seq.base_len() < doc_chars {
                 op_seq.retain((doc_chars - op_seq.base_len()) as u64);
             }
-            document = op_seq.apply(&document).expect(&format!(
-                "Could not apply operation {:?} to string with length {}.",
-                op_seq, doc_chars
-            ));
+            document = op_seq.apply(&document).unwrap_or_else(|_| {
+                panic!(
+                    "Could not apply operation {:?} to string with length {}.",
+                    op_seq, doc_chars
+                )
+            });
         }
         document
     }
@@ -188,11 +190,13 @@ fn transform_through_operations(
             let diff = my_op_seq.base_len() - their_op_seq.base_len();
             their_op_seq.retain(diff as u64);
         }
-        let (my_prime, their_prime) = my_op_seq.transform(&their_op_seq).expect(&format!(
-            "Could not transform operations {:?} on top of {:?}.",
-            &their_op_seq.ops(),
-            &my_op_seq.ops()
-        ));
+        let (my_prime, their_prime) = my_op_seq.transform(&their_op_seq).unwrap_or_else(|_| {
+            panic!(
+                "Could not transform operations {:?} on top of {:?}.",
+                &their_op_seq.ops(),
+                &my_op_seq.ops()
+            )
+        });
         transformed_my_operations.push(my_prime);
         their_op_seq = their_prime;
     }

commit 17afd79590dd209ce7cbef7d5d6b7902b5145676

Fix bug introduced by 9502c7283bb2

rust/src/types.rs
@@ -2,7 +2,6 @@
 use automerge::PatchAction;
 use operational_transform::{Operation as OTOperation, OperationSeq};
 use serde_json::Value as JSONValue;
-use tracing::warn;
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct TextDelta(pub Vec<TextOp>);
@@ -220,7 +219,7 @@ impl From<EditorTextDelta> for TextDelta {
         for ed_op in ed_delta.0 {
             let mut delta_step = TextDelta::default();
             if ed_op.range.is_empty() {
-                if ed_op.replacement.is_empty() {
+                if !ed_op.replacement.is_empty() {
                     // insert
                     delta_step.retain(ed_op.range.anchor);
                     delta_step.insert(&ed_op.replacement);