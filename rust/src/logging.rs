5c66b48915199b9937f65c14ceda660a60394409
Moritz Neeb <nt4u@kpvn.de>
2024-04-18 19:07:11 +0200

    Add missing module for logging

---
+++ rust/src/logging.rs
@@ -0,0 +1,21 @@
+use time;
+
+use tracing_subscriber::{fmt, FmtSubscriber};
+
+pub fn initialize() {
+    let timer = time::format_description::parse("[hour]:[minute]:[second]")
+        .expect("Could not create time format description");
+    let time_offset =
+        time::UtcOffset::current_local_offset().unwrap_or_else(|_| time::UtcOffset::UTC);
+    let timer = fmt::time::OffsetTime::new(time_offset, timer);
+
+    let subscriber = FmtSubscriber::builder()
+        // .pretty()
+        .with_max_level(tracing::Level::DEBUG)
+        // .with_thread_names(true)
+        .with_thread_ids(true)
+        .with_timer(timer)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Setting default log subscriber failed");
+}
