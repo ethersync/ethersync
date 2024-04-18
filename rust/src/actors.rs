7835d373402cb532798a1408503323f556fb1fd7
Moritz Neeb <nt4u@kpvn.de>
2024-04-18 17:06:32 +0200

    Simplify fuzzer

--- rust/src/actors.rs
+++ rust/src/actors.rs
@@ -93,7 +93,7 @@ impl Actor for Neovim {
 
         // TODO: There seems to be a bug when enabling multiline insertions and/or multi-line
         // deletions. Something to do with empty lines?
-        if rand::thread_rng().gen_bool(0.5) {
+        if false && rand::thread_rng().gen_bool(0.5) {
             let deletion_components = vec!["x", "dd", "vllld"];
             vim_normal_command.push_str(&random_string(
                 rand_usize_inclusive(1, 2),
@@ -102,7 +102,7 @@ impl Actor for Neovim {
         } else {
             vim_normal_command.push('i');
             //let vim_components = vec!["x", "🥕", "_", "💚"]; //, "\n"];
-            let vim_components = vec!["x", "_", "\n"];
+            let vim_components = vec!["x", "_"];
             vim_normal_command
                 .push_str(&random_string(rand_usize_inclusive(1, 10), &vim_components));
         }
