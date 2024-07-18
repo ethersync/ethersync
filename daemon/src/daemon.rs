8995d4ed9bc7609ffb0afe8865b0146f51005a9a
Moritz Neeb <nt4u@kpvn.de>
2024-07-18 17:12:03 +0200

    refactor: pull out spawning file watcher

--- daemon/src/daemon.rs
+++ daemon/src/daemon.rs
@@ -578,39 +578,7 @@ impl Daemon {
         let watcher_document_handle = document_handle.clone();
         let watcher_base_dir = base_dir.to_path_buf();
         tokio::spawn(async move {
-            let mut watcher =
-                notify::recommended_watcher(move |res: NotifyResult<notify::Event>| {
-                    futures::executor::block_on(async {
-                        match res {
-                            Ok(event) => {
-                                if event.kind.is_remove() {
-                                    println!("event: {:?}", event);
-                                    // TODO: only watch for files that we track.
-                                    for path in event.paths {
-                                        watcher_document_handle
-                                            .send_message(DocMessage::RemoveFile {
-                                                // TODO: panic if losing something
-                                                file_path: path.display().to_string(),
-                                            })
-                                            .await;
-                                    }
-                                }
-                            }
-                            Err(e) => println!("watch error: {:?}", e),
-                        }
-                    })
-                })
-                .expect("Failed to initialize file watcher");
-
-            // Add a path to be watched. All files and directories at that path and
-            // below will be monitored for changes.
-            watcher
-                .watch(&watcher_base_dir, RecursiveMode::Recursive)
-                .expect("Failed to watch directory");
-
-            loop {
-                tokio::time::sleep(Duration::from_millis(100)).await;
-            }
+            spawn_file_watcher(watcher_base_dir, watcher_document_handle).await;
         });
 
         let connection_document_handle = document_handle.clone();
@@ -626,6 +594,44 @@ impl Daemon {
         });
 
         Self { document_handle }
+    }
+}
+
+async fn spawn_file_watcher(base_dir: PathBuf, document_handle: DocumentActorHandle) {
+    // Beware of the file watching spaghetti triangle monster.
+    let mut watcher = notify::recommended_watcher(move |res: NotifyResult<notify::Event>| {
+        futures::executor::block_on(async {
+            match res {
+                Ok(event) => {
+                    if event.kind.is_remove() {
+                        println!("event: {:?}", event);
+                        for path in event.paths {
+                            document_handle
+                                .send_message(DocMessage::RemoveFile {
+                                    file_path: path
+                                        .to_str()
+                                        .expect("Failed to convert path to string")
+                                        .into(),
+                                })
+                                .await;
+                        }
+                    }
+                }
+                Err(e) => println!("watch error: {:?}", e),
+            }
+        })
+    })
+    .expect("Failed to initialize file watcher");
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher
+        .watch(&base_dir, RecursiveMode::Recursive)
+        .expect("Failed to watch directory");
+
+    // TODO: can this be event based?
+    loop {
+        tokio::time::sleep(Duration::from_millis(100)).await;
     }
 }
 
