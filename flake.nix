c79291afb599bb1124e536feabaabc3d01e273f0
blinry <mail@blinry.org>
2024-08-06 17:34:44 +0200

    Revert "Merge pull request #91 from Sohalt/main"
    This reverts commit a94e3620ec7e06702350cb6441e1d7da6b40b94f, reversing
    changes made to c7690d312b6e7109796362b208115d9e988326eb.

--- flake.nix
+++ flake.nix
@@ -8,66 +8,50 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     naersk,
     ...
   }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    lib = nixpkgs.lib;
     forAllSystems = function:
-      lib.genAttrs supportedSystems (system:
-        function (import nixpkgs {
-          inherit system;
-          overlays = [self.overlays.default];
-        }));
-  in {
-    overlays.default = final: prev: rec {
-      ethersync = (final.callPackage naersk {}).buildPackage {
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ] (system: function nixpkgs.legacyPackages.${system});
+    neovim-with-ethersync-plugin = pkgs:
+      pkgs.neovim.override
+      {
+        configure = {
+          packages.plugins = {
+            start = [
+              (pkgs.vimUtils.buildVimPlugin {
+                name = "ethersync";
+                src = ./vim-plugin;
+              })
+            ];
+          };
+          # In Nix' standard environment, we can't write to $HOME, so we need to
+          # disable swapfiles, and LSP log files.
+          customRC = ''
+            set noswapfile
+            lua vim.lsp.set_log_level("off")
+          '';
+        };
+      };
+    ethersync = pkgs:
+      (pkgs.callPackage naersk {}).buildPackage {
         src = ./daemon;
       };
-      neovim-plugin = final.vimUtils.buildVimPlugin {
-        name = "ethersync";
-        src = ./vim-plugin;
-      };
-      neovim = let
-        nvim-custom = prev.neovim.override {
-          configure = {
-            packages.plugins = {
-              start = [
-                neovim-plugin
-              ];
-            };
-            # In Nix' standard environment, we can't write to $HOME, so we need to
-            # disable swapfiles, and LSP log files.
-            customRC = ''
-              set noswapfile
-              lua vim.lsp.set_log_level("off")
-            '';
-          };
-        };
-      in
-        final.writeShellScriptBin "nvim" ''
-          PATH=${lib.makeBinPath [ethersync]}:$PATH ${nvim-custom}/bin/nvim'';
-    };
-    packages = forAllSystems (pkgs: rec {
-      inherit
-        (pkgs)
-        ethersync
-        neovim-plugin
-        neovim
-        ;
-      default = ethersync;
+  in {
+    packages = forAllSystems (pkgs: {
+      default = ethersync pkgs;
+      neovim = neovim-with-ethersync-plugin pkgs;
     });
 
     devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
-        packages = with pkgs; [cargo rustc neovim];
+        nativeBuildInputs = [pkgs.cargo pkgs.rustc (ethersync pkgs) (neovim-with-ethersync-plugin pkgs)];
       };
     });
   };
