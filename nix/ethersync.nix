d2b6d3665b82677e07d32a20a58efbb6955858ba
mangoiv <contact@mangoiv.com>
2024-08-16 12:36:34 +0200

    fix: Add SystemConfiguration frameworkCoreServices dependency to nix build and shell on darwin

--- nix/ethersync.nix
+++ nix/ethersync.nix
@@ -1,3 +1,6 @@
-{naersk-lib, ...}: naersk-lib.buildPackage {
+{naersk-lib, lib, hostPlatform, darwin, ...}: naersk-lib.buildPackage {
   src = ../daemon;
+  nativeBuildInputs = lib.optionals hostPlatform.isDarwin [
+    darwin.apple_sdk.frameworks.SystemConfiguration
+  ];
 }
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,5 +1,9 @@
 {pkgs ? import <nixpkgs> {}, lib ? pkgs.lib, ...}: pkgs.mkShell {
   packages =
     (with pkgs; [cargo rustc neovim])
-    ++ lib.optionals pkgs.hostPlatform.isDarwin [pkgs.darwin.apple_sdk.frameworks.CoreServices pkgs.libiconv];
+    ++ lib.optionals pkgs.hostPlatform.isDarwin [
+      pkgs.darwin.apple_sdk.frameworks.CoreServices
+      pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+      pkgs.libiconv
+    ];
 }
