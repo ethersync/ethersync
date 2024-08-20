312f7a6668bab227bd5d02017b267637fc12138f
Moritz Neeb <nt4u@kpvn.de>
2024-08-20 16:41:09 +0200

    WIP

--- vscode-plugin/src/extension.ts
+++ vscode-plugin/src/extension.ts
@@ -5,18 +5,18 @@ import * as path from "path"
 import * as fs from "fs"
 
 function isEthersyncEnabled(dir: string) {
-    if (fs.existsSync(path.join(dir, '.ethersync'))) {
-        return true;
+    if (fs.existsSync(path.join(dir, ".ethersync"))) {
+        return true
     }
 
-    const parentDir = path.resolve(dir, '..');
+    const parentDir = path.resolve(dir, "..")
 
     // If we are at the root directory, stop the recursion.
     if (parentDir === dir) {
-        return false;
+        return false
     }
 
-    return isEthersyncEnabled(parentDir);
+    return isEthersyncEnabled(parentDir)
 }
 
 interface Position {
@@ -47,9 +47,10 @@ interface Edit {
 let ignoreEdits = false
 let daemonRevision = 0
 let editorRevision = 0
+let t0 = Date.now()
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("Ethersync extension activated!")
+    debug("Ethersync extension activated!")
     const ethersyncClient = cp.spawn("ethersync", ["client"])
 
     ethersyncClient.on("error", (err) => {
@@ -62,7 +63,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     const connection = rpc.createMessageConnection(
         new rpc.StreamMessageReader(ethersyncClient.stdout),
-        new rpc.StreamMessageWriter(ethersyncClient.stdin),
+        new rpc.StreamMessageWriter(ethersyncClient.stdin)
     )
 
     const open = new rpc.NotificationType<{uri: string}>("open")
@@ -71,33 +72,33 @@ export function activate(context: vscode.ExtensionContext) {
 
     connection.onNotification("edit", async (edit: Edit) => {
         if (edit.delta.revision !== editorRevision) {
-            console.log(`Received edit for revision ${edit.delta.revision} (!= ${editorRevision}), ignoring`)
+            debug(`Received edit for revision ${edit.delta.revision} (!= ${editorRevision}), ignoring`)
+            return
         }
 
         daemonRevision += 1
 
-        console.log(edit)
+        debug(`Received edit ${edit.delta.revision}`)
         const uri = edit.uri
 
         const openEditor = vscode.window.visibleTextEditors.find(
-            (editor) => editor.document.uri.toString() === uri.toString(),
+            (editor) => editor.document.uri.toString() === uri.toString()
         )
-
         if (openEditor) {
             ignoreEdits = true
             for (const delta of edit.delta.delta) {
                 const range = new vscode.Range(
                     new vscode.Position(delta.range.anchor.line, delta.range.anchor.character),
-                    new vscode.Position(delta.range.head.line, delta.range.head.character),
+                    new vscode.Position(delta.range.head.line, delta.range.head.character)
                 )
                 if (openEditor) {
                     // Apply the edit if the document is open
                     await openEditor.edit((editBuilder) => {
                         editBuilder.replace(range, delta.replacement)
                     })
-                    console.log(`Edit applied to open document: ${uri.toString()}`)
+                    debug(`Edit applied to open document: ${uri.toString()}`)
                 } else {
-                    console.log(`Document not open: ${uri.toString()}`)
+                    debug(`Document not open: ${uri.toString()}`)
                 }
             }
             ignoreEdits = false
@@ -113,8 +114,20 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable)
 
+    // NOTE: We might get multiple events per document.version,
+    // as the _state_ of the document might change (like isDirty).
     disposable = vscode.workspace.onDidChangeTextDocument((event) => {
-       if (ignoreEdits) {
+        if (ignoreEdits) {
+            debug("ack")
+            return
+        }
+
+        // For some reason we get multipe events per edit caused by us.
+        // Let's actively skip the empty ones to make debugging output below less noisy.
+        if (event.contentChanges.length == 0) {
+            if (event.document.isDirty == false) {
+                debug("ignoring empty docChange. (probably saving...)")
+            }
             return
         }
 
@@ -122,8 +135,10 @@ export function activate(context: vscode.ExtensionContext) {
         if (!isEthersyncEnabled(path.dirname(filename))) {
             return
         }
-        
-        console.log(event.document.version)
+
+        // debug("event.document.version: " + event.document.version)
+        // debug("event.document.isDirty: " + event.document.isDirty)
+        // if (event.contentChanges.length == 0) { console.log(event.document) }
         for (const change of event.contentChanges) {
             let delta = {
                 range: {
@@ -133,11 +148,12 @@ export function activate(context: vscode.ExtensionContext) {
                 replacement: change.text,
             }
             let revDelta: RevisionedDelta = {delta: [delta], revision: daemonRevision}
-            let uri = "file://" + filename
+            let uri = event.document.uri.toString()
             let theEdit: Edit = {uri, delta: revDelta}
-            console.log(edit)
+            // console.log(edit)
             connection.sendNotification(edit, theEdit)
             editorRevision += 1
+            debug(`sent edit for dR ${daemonRevision} (having edR ${editorRevision})`)
         }
     })
 
@@ -145,6 +161,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     let openDisposable = vscode.workspace.onDidOpenTextDocument((document) => {
         const fileUri = document.uri.toString()
+        debug("OPEN " + fileUri)
         connection.sendNotification(open, {uri: fileUri})
     })
 
@@ -156,6 +173,11 @@ export function activate(context: vscode.ExtensionContext) {
     })
 
     context.subscriptions.push(closeDisposable)
+    debug("end of activation")
 }
 
 export function deactivate() {}
+
+function debug(text: String) {
+    console.log(Date.now() - t0 + " " + text)
+}
