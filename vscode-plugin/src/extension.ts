99e121d62274382fb1c2936c65189eb7dc90af5c
Moritz Neeb <nt4u@kpvn.de>
2024-08-28 14:35:31 +0200

    Add some code to maybe make sense of Position.character

--- vscode-plugin/src/extension.ts
+++ vscode-plugin/src/extension.ts
@@ -1,61 +1,6 @@
 import * as vscode from "vscode"
-import * as cp from "child_process"
-import * as rpc from "vscode-jsonrpc/node"
-import * as path from "path"
-import * as fs from "fs"
-var Mutex = require("async-mutex").Mutex
 
-function isEthersyncEnabled(dir: string) {
-    if (fs.existsSync(path.join(dir, ".ethersync"))) {
-        return true
-    }
-
-    const parentDir = path.resolve(dir, "..")
-
-    // If we are at the root directory, stop the recursion.
-    if (parentDir === dir) {
-        return false
-    }
-
-    return isEthersyncEnabled(parentDir)
-}
-
-interface Position {
-    line: number
-    character: number
-}
-
-interface Range {
-    start: Position
-    end: Position
-}
-
-interface Delta {
-    range: Range
-    replacement: string
-}
-
-interface RevisionedDelta {
-    delta: Delta[]
-    revision: number
-}
-
-interface Edit {
-    uri: string
-    delta: RevisionedDelta
-}
-
-class Revision {
-    daemon = 0
-    editor = 0
-}
-
-let revisions: {[filename: string]: Revision} = {}
-
-// TODO: if we load from disk, this will also cause edits :-/
-let ignoreEdits = false
-let t0 = Date.now()
-const mutex = new Mutex()
+const t0 = Date.now()
 
 function uri_to_fname(uri: string): string {
     const prefix = "file://"
@@ -65,113 +10,44 @@ function uri_to_fname(uri: string): string {
     return uri.slice(prefix.length)
 }
 
-// helpful: https://stackoverflow.com/a/54369605
-function UTF16CodeUnitOffsetToCharOffset(utf16CodeUnitOffset: number, content: string): number {
-    if (utf16CodeUnitOffset > content.length) {
-        throw new Error(
-            `Could not convert UTF-16 code unit offset ${utf16CodeUnitOffset} to char offset in string '${content}'`
-        )
-    }
-    return [...content.slice(0, utf16CodeUnitOffset)].length
-}
-
-function charOffsetToUTF16CodeUnitOffset(charOffset: number, content: string): number {
-    let utf16Offset = 0
-    let chars = [...content]
-    if (charOffset > chars.length) {
-        throw new Error(`Could not convert char offset ${charOffset} to UTF-16 code unit offset in string '${content}'`)
-    }
-    for (const char of [...content].slice(0, charOffset)) {
-        utf16Offset += char.length
-    }
-    return utf16Offset
-}
-
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
     debug("Ethersync extension activated!")
-    const ethersyncClient = cp.spawn("ethersync", ["client"])
+    const ws = vscode.workspace
+    const win = vscode.window
+    if (ws.workspaceFolders) {
+        let wsPath = ws.workspaceFolders[0].uri.path
+        const filePath = vscode.Uri.file(wsPath + "/test_position_waeCah7i.txt")
+        const filePathBefore = vscode.Uri.file(wsPath + "/test_position_waeCah7i_before.txt")
+        // const content = '01234\n🥕1234\n0🥕234\n01🥕34\n012🥕4\n0123🥕\n⛄1234\n0⛄234\n01⛄34\n012⛄4\n0123⛄\n💚1234\n0💚234\n01💚34\n012💚4\n0123💚\n'
+        const content = "01🥕34\n01🥕34\n01🥕34\n01🥕34\n01🥕34\n01🥕34\n01234\n01234\n01234\n01234\n01234\n01234\n"
+        await ws.fs.writeFile(filePath, Buffer.from(content))
+        await ws.fs.writeFile(filePathBefore, Buffer.from(content))
 
-    ethersyncClient.on("error", (err) => {
-        console.error(`Failed to start ethersync client: ${err.message}`)
-    })
+        // Close all editors from a previous run.
+        //vscode.window.visibleTextEditors.map((ed) => { ed.})
+        vscode.commands.executeCommand("workbench.action.closeActiveEditor")
 
-    ethersyncClient.on("exit", () => {
-        vscode.window.showErrorMessage("Connection to Ethersync daemon lost.")
-    })
+        const textDocument = await ws.openTextDocument(filePath)
 
-    const connection = rpc.createMessageConnection(
-        new rpc.StreamMessageReader(ethersyncClient.stdout),
-        new rpc.StreamMessageWriter(ethersyncClient.stdin)
-    )
-
-    const open = new rpc.NotificationType<{uri: string}>("open")
-    const close = new rpc.NotificationType<{uri: string}>("close")
-    const edit = new rpc.NotificationType<Edit>("edit")
-
-    connection.onNotification("edit", async (edit: Edit) => {
-        const filename = uri_to_fname(edit.uri)
-        let revision = revisions[filename]
-        if (edit.delta.revision !== revision.editor) {
-            debug(`Received edit for revision ${edit.delta.revision} (!= ${revision.editor}), ignoring`)
-            return
+        debug("Looping over offset to show line/character Position")
+        for (let i = 0; i < content.length; i++) {
+            const pos = textDocument.positionAt(i)
+            console.log(i, pos.line, pos.character)
         }
 
-        revision.daemon += 1
-
-        debug(`Received edit ${edit.delta.revision}`)
-        console.log(revisions)
-        const uri = edit.uri
-
-        const openEditor = vscode.window.visibleTextEditors.find(
-            (editor) => editor.document.uri.toString() === uri.toString()
-        )
-        if (openEditor) {
-            try {
-                await mutex.runExclusive(async () => {
-                    ignoreEdits = true
-                    for (const delta of edit.delta.delta) {
-                        // TODO: make this nicer / use the vscode interface already
-                        console.log(delta)
-                        let startLineText = openEditor.document.lineAt(delta.range.start.line).text
-                        let endLineText
-                        if (delta.range.start.line == delta.range.end.line) {
-                            endLineText = startLineText
-                        } else {
-                            endLineText = openEditor.document.lineAt(delta.range.end.line).text
-                        }
-                        const range = new vscode.Range(
-                            new vscode.Position(
-                                delta.range.start.line,
-                                charOffsetToUTF16CodeUnitOffset(delta.range.start.character, startLineText)
-                            ),
-                            new vscode.Position(
-                                delta.range.end.line,
-                                charOffsetToUTF16CodeUnitOffset(delta.range.end.character, endLineText)
-                            )
-                        )
-                        console.log(range)
-                        // TODO: this logic seems redundant and `else` will never happen. Fix later.
-                        if (openEditor) {
-                            // Apply the edit if the document is open
-                            await openEditor.edit((editBuilder) => {
-                                editBuilder.replace(range, delta.replacement)
-                            })
-                            debug(`Edit applied to open document: ${uri.toString()}`)
-                        } else {
-                            debug(`Document not open: ${uri.toString()}`)
-                        }
-                    }
-                    ignoreEdits = false
-                })
-            } catch (e) {
-                debug("promise failed")
-                console.error(e)
+        const editor = await win.showTextDocument(textDocument)
+        const nLines = (content.match(/\n/g) || []).length
+        editor.edit((editBuilder) => {
+            for (let line = 0; line < nLines; line++) {
+                const character = line % 6
+                const range = new vscode.Range(
+                    new vscode.Position(line, character),
+                    new vscode.Position(line, character)
+                )
+                editBuilder.replace(range, ".")
             }
-        }
-    })
-
-    // Start the connection
-    connection.listen()
+        })
+    }
 
     let disposable = vscode.commands.registerCommand("ethersync.helloWorld", () => {
         vscode.window.showInformationMessage("Ethersync activated!")
@@ -179,101 +55,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable)
 
-    // NOTE: We might get multiple events per document.version,
-    // as the _state_ of the document might change (like isDirty).
-    disposable = vscode.workspace.onDidChangeTextDocument((event) => {
-        if (ignoreEdits) {
-            debug("ack")
-            return
-        }
-
-        // For some reason we get multipe events per edit caused by us.
-        // Let's actively skip the empty ones to make debugging output below less noisy.
-        if (event.contentChanges.length == 0) {
-            if (event.document.isDirty == false) {
-                debug("ignoring empty docChange. (probably saving...)")
-            }
-            return
-        }
-
-        const filename = event.document.fileName
-        if (!isEthersyncEnabled(path.dirname(filename))) {
-            return
-        }
-
-        let revision = revisions[filename]
-
-        // debug("event.document.version: " + event.document.version)
-        // debug("event.document.isDirty: " + event.document.isDirty)
-        // if (event.contentChanges.length == 0) { console.log(event.document) }
-        for (const change of event.contentChanges) {
-            mutex
-                .runExclusive(() => {
-                    console.log(change)
-                    let startLineText = event.document.lineAt(change.range.start.line).text
-                    let endLineText
-                    if (change.range.start.line == change.range.end.line) {
-                        endLineText = startLineText
-                    } else {
-                        endLineText = event.document.lineAt(change.range.end.line).text
-                    }
-                    const range = new vscode.Range(
-                        new vscode.Position(
-                            change.range.start.line,
-                            UTF16CodeUnitOffsetToCharOffset(change.range.start.character, startLineText)
-                        ),
-                        new vscode.Position(
-                            change.range.end.line,
-                            UTF16CodeUnitOffsetToCharOffset(change.range.end.character, endLineText)
-                        )
-                    )
-                    console.log(change.range)
-                    console.log(range)
-                    let delta = {
-                        range,
-                        replacement: change.text,
-                    }
-                    let revDelta: RevisionedDelta = {delta: [delta], revision: revision.daemon}
-                    let uri = event.document.uri.toString()
-                    let theEdit: Edit = {uri, delta: revDelta}
-                    console.log(theEdit)
-                    // interestingly this seems to block when it can't send
-                    connection.sendNotification(edit, theEdit)
-                    revision.editor += 1
-                    debug(`sent edit for dR ${revision.daemon} (having edR ${revision.editor})`)
-                    console.log(revisions)
-                })
-                .catch((e: Error) => {
-                    debug("promise failed!")
-                    console.error(e)
-                })
-        }
-    })
-
-    context.subscriptions.push(disposable)
-
-    // TODO: check if belongs to project.
-    let openDisposable = vscode.workspace.onDidOpenTextDocument((document) => {
-        const fileUri = document.uri.toString()
-        debug("OPEN " + fileUri)
-        connection.sendNotification(open, {uri: fileUri})
-        revisions[document.fileName] = new Revision()
-        console.log(revisions)
-    })
-
-    context.subscriptions.push(openDisposable)
-
-    let closeDisposable = vscode.workspace.onDidCloseTextDocument((document) => {
-        if (!(document.fileName in revisions)) {
-            return
-        }
-        const fileUri = document.uri.toString()
-        connection.sendNotification(close, {uri: fileUri})
-
-        delete revisions[document.fileName]
-    })
-
-    context.subscriptions.push(closeDisposable)
     debug("end of activation")
 }
 
