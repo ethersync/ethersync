230438f656e4c066499b2996747d650fc2de6a7b
blinry <mail@blinry.org>
2024-09-24 12:46:02 +0200

    Add a "automerge-baker" tool to create the initial Automerge structure

---
+++ tools/automerge-baker/src/main.rs
@@ -0,0 +1,13 @@
+//! A helper tool to pre-bake the initial state of the Automerge document.
+//! See https://automerge.org/docs/cookbook/modeling-data/#setting-up-an-initial-document-structure
+
+use automerge::{transaction::Transactable, AutoCommit, ObjType};
+
+fn main() {
+    let mut doc = AutoCommit::new();
+    doc.put_object(automerge::ROOT, "files", ObjType::Map)
+        .expect("Failed to initialize files Map object");
+    doc.put_object(automerge::ROOT, "states", ObjType::Map)
+        .expect("Failed to initialize states Map object");
+    println!("{:?}", doc.save());
+}
