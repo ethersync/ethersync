3b50a760f69e2c80fbd837362b02a8c5cd13ab08
blinry <mail@blinry.org>
2025-01-14 11:42:05 +0100

    reuse: Add headers to remaining daemon files

--- daemon/src/path.rs
+++ daemon/src/path.rs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2024 blinry <mail@blinry.org>
+// SPDX-FileCopyrightText: 2024 zormit <nt4u@kpvn.de>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use anyhow::{bail, Context};
 use automerge::Prop;
 use derive_more::{AsRef, Deref, Display};
--- daemon/src/sandbox.rs
+++ daemon/src/sandbox.rs
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: 2024 blinry
-// SPDX-FileCopyrightText: 2024 zormit
+// SPDX-FileCopyrightText: 2024 blinry <mail@blinry.org>
+// SPDX-FileCopyrightText: 2024 zormit <nt4u@kpvn.de>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
