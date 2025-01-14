443f35b0508fa1cbece94ec64a82cda8a5399103
blinry <mail@blinry.org>
2025-01-14 11:24:31 +0100

    reuse: Add headers to Nix scripts

--- contrib/nix/nvim-ethersync.nix
+++ contrib/nix/nvim-ethersync.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 MangoIV <contact@mangoiv.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 {vimUtils, ...}:
 vimUtils.buildVimPlugin {
   name = "ethersync";
--- contrib/nix/overlay.nix
+++ contrib/nix/overlay.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 MangoIV <contact@mangoiv.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 (final: prev: let
   packages = import ./default.nix {pkgs = final;};
 in {
