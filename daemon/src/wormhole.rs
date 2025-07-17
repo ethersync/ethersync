9aad42f01f0b67786d63e046b989d455659a2ecb
blinry <mail@blinry.org>
2025-07-17 15:37:10 +0200

    daemon: After wormhole sharing succeeded/failed, print a new join code

--- daemon/src/wormhole.rs
+++ daemon/src/wormhole.rs
@@ -5,26 +5,33 @@
 
 use anyhow::Result;
 use magic_wormhole::{transfer, AppID, Code, MailboxConnection, Wormhole};
-use std::str::FromStr;
+use std::{str::FromStr, time::Duration};
+use tokio::time::sleep;
 use tracing::{info, warn};
 
 pub async fn put_secret_address_into_wormhole(address: &str) {
     let config = transfer::APP_CONFIG.id(AppID::new("ethersync"));
+    let payload: Vec<u8> = address.into();
 
-    let mailbox_connection = MailboxConnection::create(config.clone(), 2).await.unwrap();
-    let code = mailbox_connection.code().clone();
-
-    info!(
-        "\n\tTo connect to you, another person can use this one-time join code:\n\n\tethersync join {}\n",
-        &code
-    );
-
-    let payload = address.into();
     tokio::spawn(async move {
-        if let Ok(mut wormhole) = Wormhole::connect(mailbox_connection).await {
-            let _ = wormhole.send(payload).await;
-        } else {
-            warn!("Failed to share secret address. Did your peer mistype the join code?");
+        loop {
+            let mailbox_connection = MailboxConnection::create(config.clone(), 2).await.unwrap();
+            let code = mailbox_connection.code().clone();
+
+            info!(
+                "\n\tOne other person can use this to connect to you:\n\n\tethersync join {}\n",
+                &code
+            );
+
+            if let Ok(mut wormhole) = Wormhole::connect(mailbox_connection).await {
+                let _ = wormhole.send(payload.clone()).await;
+            } else {
+                warn!("Failed to share secret address. Did your peer mistype the join code?");
+            }
+
+            // Print a new join code in the next iteration of the foor loop, to allow more people
+            // to join.
+            sleep(Duration::from_millis(500)).await;
         }
     });
 }
