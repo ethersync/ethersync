702c3a7a6231996e02b4fe34e039a5f396146290
blinry <mail@blinry.org>
2025-07-27 16:13:18 +0200

    Offer to add .ethersync to gitignore, if it should be but isn't

--- daemon/src/config.rs
+++ daemon/src/config.rs
@@ -131,10 +131,10 @@ pub fn has_git_remote(path: &Path) -> bool {
     false
 }
 
-pub fn ethersync_directory_is_ignored(path: &Path) -> bool {
+pub fn ethersync_directory_should_be_ignored_but_isnt(path: &Path) -> bool {
     if let Ok(repo) = find_git_repo(path) {
         let ethersync_dir = path.join(CONFIG_DIR);
-        return repo
+        return !repo
             .is_path_ignored(ethersync_dir)
             .expect("Should have been able to determine ignore state for {path}");
     }
@@ -164,4 +164,23 @@ fn global_git_username() -> Result<String> {
 
 fn find_git_repo(path: &Path) -> Result<git2::Repository, git2::Error> {
     git2::Repository::discover(path)
+}
+
+pub fn add_ethersync_to_global_gitignore() -> Result<()> {
+    let home_dir = std::env::home_dir().expect("Could not find home directory");
+    let git_config_dir = home_dir.join(".config/git");
+    let ignore_file_path = git_config_dir.join("ignore");
+
+    sandbox::create_dir_all(&git_config_dir, &git_config_dir)?;
+    let bytes_in =
+        sandbox::read_file(&git_config_dir, &ignore_file_path).unwrap_or("".to_string().into());
+    let mut content = std::str::from_utf8(&bytes_in)?.to_string();
+    if !content.is_empty() && !content.ends_with("\n") {
+        content.push('\n');
+    }
+    content.push_str(".ethersync/\n");
+    let bytes_out = content.as_bytes();
+    sandbox::write_file(&git_config_dir, &ignore_file_path, bytes_out)?;
+
+    Ok(())
 }
