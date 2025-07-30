ffafd904f90850b4c1e3dd0f8f2d4d048c8e3ad4
blinry <mail@blinry.org>
2025-07-30 16:24:32 +0200

    100 ms after the last watcher event, do a full rescan
    This is so that we won't miss any file changes - the watcher isn't
    necessarily exhaustive.
    
    Fixes #180.

--- daemon/src/daemon.rs
+++ daemon/src/daemon.rs
@@ -48,6 +48,7 @@ pub enum DocMessage {
     },
     FromEditor(EditorId, String),
     FromWatcher(WatcherEvent),
+    RescanFiles,
     Persist,
     RandomEdit,
     ReceiveSyncMessage {
@@ -73,6 +74,7 @@ impl fmt::Debug for DocMessage {
                 format!("open/close/edit/... message from editor #{id}")
             }
             DocMessage::FromWatcher(_) => "watcher event".to_string(),
+            DocMessage::RescanFiles => "rescan".to_string(),
             DocMessage::Persist => "persist".to_string(),
             DocMessage::RandomEdit => "random edit".to_string(),
             DocMessage::ReceiveSyncMessage { .. } => "<automerge internal sync rcv>".to_string(),
@@ -187,6 +189,9 @@ impl DocumentActor {
             }
             DocMessage::FromWatcher(watcher_event) => {
                 self.handle_watcher_event(watcher_event).await;
+            }
+            DocMessage::RescanFiles => {
+                self.read_current_content_from_dir(false);
             }
             DocMessage::Persist => {
                 let persistence_file = self.base_dir.join(".ethersync/doc");
@@ -872,13 +877,45 @@ impl Daemon {
     }
 }
 
+// Spawn a file watcher and feed its events to the document_handle.
+// In addition, a short timeout after the last event, do a full re-scan, so that we don't miss any
+// file changes - the watcher isn't necessarily exhaustive.
 async fn spawn_file_watcher(base_dir: &Path, document_handle: DocumentActorHandle) {
     let mut watcher = Watcher::new(base_dir);
+
     tokio::spawn(async move {
-        while let Some(watcher_event) = watcher.next().await {
-            document_handle
-                .send_message(DocMessage::FromWatcher(watcher_event))
-                .await;
+        let debounce_duration = Duration::from_millis(100);
+
+        let debounce_timer = tokio::time::sleep(debounce_duration);
+        // Sleep does not implement the Unpin trait, so in order to use it with select!, we have to
+        // pin it first (according to the documentation).
+        tokio::pin!(debounce_timer);
+
+        let mut rescan_required = false;
+
+        loop {
+            tokio::select! {
+                maybe_event = watcher.next() => {
+                    if let Some(watcher_event) = maybe_event {
+                        document_handle
+                            .send_message(DocMessage::FromWatcher(watcher_event))
+                            .await;
+
+                        debounce_timer.as_mut().reset(tokio::time::Instant::now() + debounce_duration);
+                        rescan_required = true;
+                    } else {
+                        // Watcher terminated. Seems we're shutting down.
+                        return;
+                    }
+                }
+
+                _ = &mut debounce_timer, if rescan_required => {
+                    document_handle
+                        .send_message(DocMessage::RescanFiles)
+                        .await;
+                    rescan_required = false;
+                }
+            }
         }
     });
 }
