0a336699b014c4ae4a8374f4206ea79d1846fd9b
blinry <mail@blinry.org>
2025-08-06 14:32:42 +0200

    Add SPDX header to peer/sync.rs

--- daemon/src/peer/sync.rs
+++ daemon/src/peer/sync.rs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 blinry <mail@blinry.org>
+// SPDX-FileCopyrightText: 2025 zormit <nt4u@kpvn.de>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use crate::daemon::{DocMessage, DocumentActorHandle};
 use crate::types::EphemeralMessage;
 use anyhow::{bail, Context, Result};
