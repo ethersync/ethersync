cbf975432707b48aad31111b74f9c2bc50fb03fb
blinry <mail@blinry.org>
2025-08-20 15:03:53 +0200

    Remove Nix flake and disable Nix tests in CI
    Now that Ethersync is packaged in nixpkgs, if people want to include it
    in their Nix expresssions, they can use or modify that official package.
    
    The CI tests always took 10 minutes, which slowed down development.

--- contrib/nix/nvim-ethersync.nix
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2024 MangoIV <contact@mangoiv.com>
-#
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-{vimUtils, ...}:
-vimUtils.buildVimPlugin {
-  name = "ethersync";
-  src = ../../nvim-plugin;
-}
