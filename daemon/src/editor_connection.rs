05053100a0ea7a73af834240435ca97981299be5
winniehell <git@winniehell.de>
2025-09-28 22:24:33 +0200

    Move FileUri from path to editor connection

--- daemon/src/editor_connection.rs
+++ daemon/src/editor_connection.rs
@@ -3,19 +3,58 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::{collections::HashMap, path::PathBuf};
-
-use tracing::debug;
-
 use crate::{config, ot::OTServer, sandbox};
+use anyhow::bail;
+use derive_more::Deref;
+use std::{collections::HashMap, path::PathBuf};
+use tracing::debug;
+use url::Url;
 
 use ethersync_shared::{
-    path::{AbsolutePath, FileUri, RelativePath},
+    path::{AbsolutePath, RelativePath},
     types::{
         ComponentMessage, CursorState, EditorProtocolMessageError, EditorProtocolMessageFromEditor,
         EditorProtocolMessageToEditor, RevisionedEditorTextDelta,
     },
 };
+
+// TODO: Wrap the newtype around url::Url instead?
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deref)]
+#[must_use]
+pub struct FileUri(String);
+
+impl FileUri {
+    pub fn from_absolute_path(path: &AbsolutePath) -> Self {
+        Self::try_from(format!("file://{}", path.display()))
+            .expect("Should be able to create File URI from absolute path")
+    }
+
+    pub fn to_absolute_path(&self) -> AbsolutePath {
+        let path_buf = Url::parse(self)
+            .expect("Should be able to parse file:// URL as Url")
+            .to_file_path()
+            .expect("Should be able to convert Url to PathBuf");
+        AbsolutePath::try_from(path_buf).expect("File URI should contain an absolute path")
+    }
+}
+
+impl TryFrom<String> for FileUri {
+    type Error = anyhow::Error;
+
+    fn try_from(string: String) -> Result<Self, Self::Error> {
+        // TODO: Could be written simpler?
+        if string.starts_with("file:///") {
+            // Use the url crate to properly URL encode the path (spaces should be "%20", for example).
+            Ok(Self(
+                Url::parse(&string)
+                    .expect("Should be able to parse file:// URL")
+                    .to_string(),
+            ))
+        } else {
+            bail!("File URI '{}' does not start with 'file:///'", string);
+        }
+    }
+}
 
 /// Represents a connection to an editor. Handles the OT. To keep the code testable and sync, we do
 /// the actual sending of messages in the daemon, and the functions here just *calculate* them.
@@ -58,9 +97,9 @@ impl EditorConnection {
                     debug!("Applying incoming CRDT patch for {file_path}");
                     let rev_text_delta_for_editor = ot_server.apply_crdt_change(delta);
 
-                    let uri = AbsolutePath::from_parts(&self.base_dir, file_path)
-                        .expect("Should be able to construct absolute URI")
-                        .to_file_uri();
+                    let self1 = &AbsolutePath::from_parts(&self.base_dir, file_path)
+                        .expect("Should be able to construct absolute URI");
+                    let uri = FileUri::from_absolute_path(self1);
 
                     vec![EditorProtocolMessageToEditor::Edit {
                         uri: uri.to_string(),
@@ -76,9 +115,9 @@ impl EditorConnection {
                 cursor_id,
                 cursor_state,
             } => {
-                let uri = AbsolutePath::from_parts(&self.base_dir, &cursor_state.file_path)
-                    .expect("Should be able to construct absolute URI")
-                    .to_file_uri();
+                let self1 = &AbsolutePath::from_parts(&self.base_dir, &cursor_state.file_path)
+                    .expect("Should be able to construct absolute URI");
+                let uri = FileUri::from_absolute_path(self1);
 
                 vec![EditorProtocolMessageToEditor::Cursor {
                     userid: cursor_id.clone(),
@@ -204,9 +243,9 @@ impl EditorConnection {
                 let (delta_for_crdt, rev_deltas_for_editor) =
                     ot_server.apply_editor_operation(rev_delta);
 
-                let uri = AbsolutePath::from_parts(&self.base_dir, &relative_path)
-                    .expect("Should be able to construct absolute URI")
-                    .to_file_uri();
+                let self1 = &AbsolutePath::from_parts(&self.base_dir, &relative_path)
+                    .expect("Should be able to construct absolute URI");
+                let uri = FileUri::from_absolute_path(self1);
 
                 let messages_to_editor = rev_deltas_for_editor
                     .into_iter()
@@ -252,6 +291,7 @@ mod tests {
     use super::*;
     use ethersync_shared::types::factories::*;
     use pretty_assertions::assert_eq;
+    use std::path::Path;
     use temp_dir::TempDir;
 
     #[test]
@@ -335,5 +375,33 @@ mod tests {
                                                             // same.
             }]
         );
+    }
+
+    #[test]
+    fn test_file_path_for_uri_works() {
+        let base_dir = Path::new("/an/absolute/path");
+
+        let file_paths = vec!["file1", "sub/file3", "sub"];
+        for &expected in &file_paths {
+            let uri =
+                FileUri::try_from(format!("file://{}/{}", base_dir.display(), expected)).unwrap();
+            let absolute_path = uri.to_absolute_path();
+            let relative_path = RelativePath::try_from_absolute(base_dir, &absolute_path).unwrap();
+
+            assert_eq!(RelativePath::new(expected), relative_path);
+        }
+    }
+
+    #[test]
+    fn test_uri_encoding_works_with_spaces() {
+        let uri = FileUri::try_from("file:///a/b/file with spaces".to_string()).unwrap();
+        assert_eq!("file:///a/b/file%20with%20spaces", uri.0);
+    }
+
+    #[test]
+    fn test_uri_decoding_works_with_spaces() {
+        let uri = FileUri::try_from("file:///a/b/file with spaces".to_string()).unwrap();
+        let absolute_path = AbsolutePath::try_from("/a/b/file with spaces").unwrap();
+        assert_eq!(absolute_path, uri.to_absolute_path());
     }
 }
